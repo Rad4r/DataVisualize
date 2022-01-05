9e5c26177fb04dfef51c1dc3a44aad3ab4585e6c
V <hanimohammad00@gmail.com>
2022-01-05 19:12:51 +0400

    Base App completed

--- Data Visualize/Assets/GameInfoScreen.cs
+++ Data Visualize/Assets/GameInfoScreen.cs
@@ -1,30 +1,59 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameInfoScreen : MonoBehaviour
 {
 
+    [Header("Info Objects")]
     [SerializeField] private TextMeshProUGUI gameNameUI;
     [SerializeField] private TextMeshProUGUI versionNumbUI;
     [SerializeField] private TextMeshProUGUI dateUI;
     [SerializeField] private TextMeshProUGUI highScoreUI;
     [SerializeField] private Image gameIcon;
     [SerializeField] private ImageDetail[] images;
+    [SerializeField] private TextMeshProUGUI gameFound;
     
-    public void UpdateGameInfo(string gameName) //could just use the gameObject.Name
+    [Header("Screen")] 
+    [SerializeField] private GameObject menuScreen;
+    private LoadObject gameData;
+
+    private void Update()
     {
-        LoadObject gameData = FindObjectOfType<LoadGameInfo>().FindData(gameName);
+        if (Input.GetButtonDown("Cancel"))
+        {
+            menuScreen.SetActive(true);
+            gameObject.SetActive(false);
+            Invoke("ResetButtonSelection", .1f);
+        }
+    }
+
+    private void ResetButtonSelection()
+    {
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("GamesContainer").transform.GetChild(0).gameObject);
+    }
+
+    public void UpdateGameInfo()
+    {
+        string game = EventSystem.current.currentSelectedGameObject.name;
+        gameData = FindObjectOfType<LoadGameInfo>().FindData(game);
+        menuScreen.SetActive(false);
+        gameObject.SetActive(true);
 
         gameNameUI.text = gameData.gameName;
         versionNumbUI.text = gameData.versionNumber;
         dateUI.text = gameData.dateTime;
         highScoreUI.text = gameData.highScore.ToString();
-
+        if (gameData.infoFound)
+            gameFound.text = "Info found";
+        else
+            gameFound.text = "Info unavailable";
+        
         foreach (ImageDetail img in images)
         {
-            if (img.imageName == gameName)
+            if (img.imageName == game)
             {
                 gameIcon.sprite = img.imageSprite;
                 break;
--- Data Visualize/Assets/LoadGameInfo.cs
+++ Data Visualize/Assets/LoadGameInfo.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.IO;
 using System;
+using TMPro;
 
 public class LoadGameInfo : MonoBehaviour
 {
     System.Threading.Thread m_LoadThread = null;
+
+    [SerializeField] private TextMeshProUGUI filePath;
 
     string m_DataPath;
 
@@ -15,10 +18,9 @@ public class LoadGameInfo : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         m_DataPath = Application.persistentDataPath + "/../YellowRidersHospitalGame.txt";
-
+        filePath.text = "File Path: " + m_DataPath;
         //m_LoadThread = new System.Threading.Thread(LoadThread);
     }
-
 
     public LoadObject FindData(string gameName) // need the string to check
     {
@@ -29,10 +31,14 @@ public class LoadGameInfo : MonoBehaviour
             for (int i = 0; i <= fileLines.Length -1 ; i++) //check only once in the beginning
             {
                 if (fileLines[i].Contains(gameName))
-                    return JsonUtility.FromJson<LoadObject>(fileLines[i]);
+                {
+                    LoadObject gameInfo = JsonUtility.FromJson<LoadObject>(fileLines[i]);
+                    gameInfo.infoFound = true;
+                    return gameInfo;
+                }
             }
         }
-        return new LoadObject(){gameName = "default", dateTime = "00/00/00", highScore = 0,versionNumber = "0"};
+        return new LoadObject(){gameName = "default", dateTime = "00/00/00", highScore = 0,versionNumber = "0", infoFound = false};
     }
     
 }
@@ -44,6 +50,7 @@ public struct LoadObject
     public string versionNumber;
     public string dateTime;
     public float highScore;
+    public bool infoFound;
 }
 
 
